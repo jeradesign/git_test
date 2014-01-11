2b08fc30d90c1155f426669a4235ea39df01d883
Joel Bremson <joel3000@gmail.com>
2014-01-11 13:04:39 -0800

    Added a @koan decorator to streamline the koan build process and remove drudgery tasks.

--- koans.py
+++ koans.py
@@ -3,6 +3,16 @@
 __author__ = 'joelbremson'
 __date__ = "1/7/14"
 
+"""How to write a koan.
+
+1. Start the koan with the @koan decorator. This will print a header for you and keep track of the koan state.
+2. Name your koan 'koan_<int>' where <int> is the sequence number of your koan.
+3. Do your work in the koan and clean up so that cwd is in the right place when you exit.
+4. If the koan is passed return True, otherwise return False.
+
+To start fresh rm the .koan_state file in ./git_test  .
+"""
+
 import os
 import pickle
 import subprocess
@@ -11,48 +21,49 @@ import re
 #Test file for git koans
 
 class State:
+    """Set up koan system. Check for prior use so user can continue without
+    restart. This will only work if the user hasn't altered the archive state."""
 
     keep = {'counter':1}
 
-    def __init__(self):
-        """Set up koan system. Check for prior use so user can continue without
-        restart. This will only work if the user hasn't altered the archive state."""
 
+    print "Init called.\n"
+    try:
+        f = open("../git_test/.koans_state","r")
+        inval = pickle.load(f)
+        keep['counter'] = inval['counter']
 
-        try:
-            f = open("../git_test/.koans_state")
-            inval = pickle.load(f)
-            State.keep['counter'] = inval['counter']
+    except (AttributeError,IOError):
+        # no prior state to return to.
+        print "In exception of __init__"
 
-        except (AttributeError,IOError):
-            # not prior state to return to.
-            print "In exception of __init__"
+        f = open("../git_test/.koans_state","w")
+        pickle.dump(keep,f)
+        f.close()
 
-            f = open("../git_test/.koans_state","w")
-            pickle.dump(State.keep,f)
-            f.close()
 
     @classmethod
     def inc_counter(cls):
-        State.keep['counter'] += 1
+        cls.keep['counter'] += 1
+        cls.save_state()
 
     @classmethod
     def save_state(cls):
         f = open("../git_test/.koans_state","w")
-        pickle.dump(State.keep,f)
+        pickle.dump(cls.keep,f)
         f.close()
 
     @classmethod
     def get_counter(cls):
         """Returns the value of counter."""
-        return State.keep['counter']
+        return cls.keep['counter']
 
 
 def reset():
     print ("Resetting koans to initial state...")
     # make this safer
     cmd("rm -rf ../git_test/work/")
-    cmd("rm -rf ../git_test/.koans_state")
+    #cmd("rm -rf ../git_test/.koans_state")
     cmd("mkdir ../git_test/work/")
     cmd("touch ../git_test/work/.empty")
 
@@ -70,24 +81,39 @@ def cmd(cmd):
     return out
     #return subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=False);
 
+def koan(fxn):
+    """Prints koan header and increments state counter (given the koan is passed)."""
+    def new_fxn(*args,**kwargs):
+        header = kwargs.get('header',True)
+        if header:
+            print "\n\n********  Koan " + str(State.get_counter()) + "  ********\n\n"
+        success = fxn()
+        if success: # success
+            print "\n\nEnlightenment Achieved!"
+            State.inc_counter()
+        else: # failed
+            print ("\n\nThrough failure learning is achieved. Try it again.\n\n")
+            globals()[fxn.__name__](header=False)
+    return new_fxn
 
+@koan
 def koan_1():
-    print State.get_counter()
+    """Init the first repo."""
+    retval = False
     out =  raw_input("Koan 1: Init git in the /work directory... (hint: git init ./work)\n>>")
-    retval = cmd(out)
+    cmd_ret = cmd(out)
 
     # check to see if there is a .git dir in work...
 
     if(os.path.isdir("./work/.git")):
-        print ("\n\nInit enlightenment attained. On to the next koan! \n\n")
-        State.inc_counter()
-    else:
-        print ("\n\nThrough failure learning is achieved. Try it again.\n\n")
-        koan_1();
+        retval = True
+    return retval
 
+@koan
 def koan_2():
-    print State.get_counter()
+    """Add a file."""
     cwd = os.getcwd()
+    ret_val = False
     final =  cwd.split("/")[-1]
     if not final == "work":
         os.chdir("./work")
@@ -99,35 +125,43 @@ def koan_2():
         ret = cmd(out)
         git_status = cmd("git status")
         out = re.search("new file:\s+foo",git_status)
-        if out==None:
-            print "\n\nTry again.\n\n"
-            koan_2()
-        print " File added to the repo. Now we will go to commit it in Koan 3.\n\n"
-        os.chdir("..")
-        State.inc_counter()
 
-    else:
-        print "Try again.\n\n"
-        koan_2()
+        if out.group():
+            ret_val = True
+            print " File added to the repo. Now we will go to commit it in Koan 3.\n\n"
+            os.chdir("..")
+    return ret_val
 
-
+@koan
 def koan_3():
     """Commit file."""
-    print State.get_counter()
-    print "\n\nKoan 3: Now we will commit the new file.\n\n>>"
-
-print "Welcome to git-koans..."
-reset()
+    ret_val = False
+    print "\n\nNow we will commit the new file.\n\n>>"
+    return True
 
 
-# this should store state so user doesn't have to repeat with restart.
-# iterate over koans using the symbol table
-koans = [k for k in dir() if 'koan_' in k]
-
-for koan in koans:
-    locals()[koan]()
 
 
-#git --git-dir=/Users/joelbremson/code/git_koans/.git --work-tree=./git_koans/ status
+if __name__ == "__main__":
+    print "Welcome to git-koans...\n"
+    if State.get_counter()==1:
+        reset()
+    else:
+        print "\n\nContinuing from koan " + str(State.get_counter()) + "\n\n"
+
+
+    # this should store state so user doesn't have to repeat with restart.
+    # iterate over koans using the symbol table
+    koans = [k for k in dir() if 'koan_' in k]
+
+    for koan in sorted(koans):
+        out = re.search("\d+$",koan)
+        
+        if int(out.group(0)) < State.get_counter():
+            continue
+        locals()[koan]()
+
+
+    #git --git-dir=/Users/joelbremson/code/git_koans/.git --work-tree=./git_koans/ status
 
 
